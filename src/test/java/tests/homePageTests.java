456f529e6b5cf5b3b1bfb7c3063d82b8fe8d48fe
Labone <labs345@gmail.com>
2026-06-21 17:56:49 +0530

    continue shopping

--- src/test/java/tests/homePageTests.java
+++ src/test/java/tests/homePageTests.java
@@ -1,5 +1,11 @@
 package tests;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,6 +17,25 @@ public class homePageTests extends testBaseClass {
 	void HomepageTests() {
 		amazonHomePage homePage = new amazonHomePage(driver);
 		resultsPage result = new resultsPage(driver);
+		
+		try {
+		    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+		    WebElement continueShopping = wait.until(
+		        ExpectedConditions.elementToBeClickable(
+		            By.xpath("//button[contains(text(),'Continue shopping')]")
+		        )
+		    );
+
+		    continueShopping.click();
+
+		} catch (Exception e) {
+		    System.out.println("Continue Shopping page not displayed");
+		}
+		
+		
+		
+		
 		homePage.fillSearchBox("Kurta");
 		homePage.searchItem();
 		

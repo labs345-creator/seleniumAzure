26c024b04ebf79a62545277b7a70cfbedfceb772
Labone <labs345@gmail.com>
2026-06-21 18:20:49 +0530

    hello world

--- src/test/java/pageObjects/productPage.java
+++ src/test/java/pageObjects/productPage.java
@@ -1,8 +1,13 @@
 package pageObjects;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class productPage extends pageObjectBase {
 
@@ -13,8 +18,9 @@ public class productPage extends pageObjectBase {
 	@FindBy(xpath="//span[@id='productTitle']") WebElement addedProduct;
 	
 	public boolean productAdded(String str) {
-		String text = addedProduct.getText();
-		
+	//	String text = addedProduct.getText();
+		WebDriverWait myWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		String text =	myWait.until(ExpectedConditions.presenceOfElementLocated((By) addedProduct)).getText();
 		if(text.contains(str)) {
 			return true;
 		}
